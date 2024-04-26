aff6f827b63dd0d878095d32fb06ca3d290d8500
ilham05 <ilhammaulana05303005@gmail.com>
2024-04-26 09:40:13 +0700

    del reload win

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -102,7 +102,6 @@ export default {
                     await this.$router.replace({
                         name: 'login'
                     });
-                    await window.location.reload();
             } catch (error) {
                 console.log(error)
             }
